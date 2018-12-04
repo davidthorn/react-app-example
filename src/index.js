a5c631a301e2da2a4749b030fb5536b9eae1a8ab
David Thorn <david.thorn221278@googlemail.com>
2018-12-05 00:56:04 +0100

    created basic example app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Navbar() {
+    return <div className="Navbar"></div>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Greeting extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = { 
+            counting: props.shouldStartTimer,
+            time: new Date().toLocaleTimeString() , 
+            message: this.props.message
+         }
+    }
+
+    handleClick(e) {
+        e.preventDefault()
+        if(this.handle !== undefined) {
+            clearInterval(this.handle)
+            this.handle = undefined
+            this.setState({counting: false })
+        } else {
+            this.startTimer()
+        }
+    }
+
+    startTimer() {
+        this.handle = setInterval(() => {
+            this.setState({
+                time: new Date().toLocaleTimeString(),
+                counting: true
+            })
+       }, 1000)
+    }
+
+    componentWillMount() {
+        if(this.state.counting) {
+            this.startTimer()
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                <h1>Hi there, {this.state.message}</h1>
+                <h3>The time currently is:</h3>
+                <p>
+                    {this.state.time}
+                </p>
+                <div className="buttonWrapper">
+                <button onClick={this.handleClick.bind(this)}>{ this.state.counting ? "Stop" : "Start" }</button>
+                </div>
+                
+            </div>
+            
+        )
+    }
+    
+}
+
+function David(props) {
+    return <Greeting message={props.name} shouldStartTimer={true}/>
+}
+
+ReactDOM.render(<David name="David"/>, document.getElementById('root'))
